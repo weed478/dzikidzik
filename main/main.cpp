b71b8d6ba4e787b3a89f3910812b9e86d504b929
Carbon225 <carbon225@protonmail.com>
2024-05-24 17:44:39 +0200

    format

--- main/main.cpp
+++ main/main.cpp
@@ -15,52 +15,57 @@ static const char TAG[] = "main";
 static constexpr int kTensorArenaSize = 2000;
 static uint8_t tensor_arena[kTensorArenaSize];
 
-extern "C" void app_main(void) {
-  // Map the model into a usable data structure. This doesn't involve any
-  // copying or parsing, it's a very lightweight operation.
-  const tflite::Model *model = tflite::GetModel(g_model);
-  if (model->version() != TFLITE_SCHEMA_VERSION) {
-    ESP_LOGE(TAG, "Model provided is schema version %ld not equal to supported "
-                  "version %d.", model->version(), TFLITE_SCHEMA_VERSION);
-    return;
-  }
-
-  // Pull in only the operation implementations we need.
-  tflite::MicroMutableOpResolver<8> resolver;
-  resolver.AddQuantize();
-  resolver.AddConv2D();
-  resolver.AddRelu();
-  resolver.AddAdd();
-  resolver.AddDepthwiseConv2D();
-  resolver.AddMean();
-  resolver.AddReshape();
-  resolver.AddSoftmax();
-
-  // Build an interpreter to run the model with.
-  tflite::MicroInterpreter interpreter(
-      model, resolver, tensor_arena, kTensorArenaSize);
-
-  // Allocate memory from the tensor_arena for the model's tensors.
-  TfLiteStatus allocate_status = interpreter.AllocateTensors();
-  if (allocate_status != kTfLiteOk) {
-    ESP_LOGE(TAG, "AllocateTensors() failed");
-    return;
-  }
-
-  TfLiteTensor *input = interpreter.input(0);
-  TfLiteTensor *output = interpreter.output(0);
-
-  for (;;)
-  {
-    int64_t start_us = esp_timer_get_time();
-    TfLiteStatus invoke_status = interpreter.Invoke();
-    int64_t latency = esp_timer_get_time() - start_us;
-    if (invoke_status != kTfLiteOk) {
-      ESP_LOGE(TAG, "Invoke failed");
-      return;
+extern "C" void app_main(void)
+{
+    // Map the model into a usable data structure. This doesn't involve any
+    // copying or parsing, it's a very lightweight operation.
+    const tflite::Model *model = tflite::GetModel(g_model);
+    if (model->version() != TFLITE_SCHEMA_VERSION)
+    {
+        ESP_LOGE(TAG, "Model provided is schema version %ld not equal to supported "
+                      "version %d.",
+                 model->version(), TFLITE_SCHEMA_VERSION);
+        return;
     }
-    int64_t fps = 1000000 / latency;
-    ESP_LOGI(TAG, "Latency: %lld ms, FPS: %lld", latency / 1000, fps);
-    vTaskDelay(pdMS_TO_TICKS(500));
-  }
+
+    // Pull in only the operation implementations we need.
+    tflite::MicroMutableOpResolver<8> resolver;
+    resolver.AddQuantize();
+    resolver.AddConv2D();
+    resolver.AddRelu();
+    resolver.AddAdd();
+    resolver.AddDepthwiseConv2D();
+    resolver.AddMean();
+    resolver.AddReshape();
+    resolver.AddSoftmax();
+
+    // Build an interpreter to run the model with.
+    tflite::MicroInterpreter interpreter(
+        model, resolver, tensor_arena, kTensorArenaSize);
+
+    // Allocate memory from the tensor_arena for the model's tensors.
+    TfLiteStatus allocate_status = interpreter.AllocateTensors();
+    if (allocate_status != kTfLiteOk)
+    {
+        ESP_LOGE(TAG, "AllocateTensors() failed");
+        return;
+    }
+
+    TfLiteTensor *input = interpreter.input(0);
+    TfLiteTensor *output = interpreter.output(0);
+
+    for (;;)
+    {
+        int64_t start_us = esp_timer_get_time();
+        TfLiteStatus invoke_status = interpreter.Invoke();
+        int64_t latency = esp_timer_get_time() - start_us;
+        if (invoke_status != kTfLiteOk)
+        {
+            ESP_LOGE(TAG, "Invoke failed");
+            return;
+        }
+        int64_t fps = 1000000 / latency;
+        ESP_LOGI(TAG, "Latency: %lld ms, FPS: %lld", latency / 1000, fps);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
 }
